907ae78ca5b58dace997985ef7827938646fd969
mahian ahmad <mahianahamd9@gmail.com>
2022-12-29 18:21:58 +0600

    collaps navbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,13 +13,9 @@ const Home = () => {
                                     <h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">Organize your work and life, finally.</h1>
                                     <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">Become focused, organized, and calm with Todoist. The world’s #1 task manager and to-do list app.</p>
                                     <div class="mt-8 flex gap-x-4 sm:justify-center">
-                                        <Link href="#" class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
+                                        <Link to="add-task" class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
                                             Get started
                                             <span class="text-indigo-200" aria-hidden="true">&rarr;</span>
-                                        </Link>
-                                        <Link href="#" class="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                                            Live demo
-                                            <span class="text-gray-400" aria-hidden="true">&rarr;</span>
                                         </Link>
                                     </div>
                                 </div>
--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -1,9 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { authContext } from '../../context/UserContext';
 
 const Nav = () => {
     const {user, logOut} = useContext(authContext);
+    const [open, setOpen] = useState(false);
+    const collaps = ()=>{
+        if(open){
+            setOpen(!open)
+        }else{
+            setOpen(true)
+        }
+    }
     return (
         <div className="bg-gray-900 px-4">
             <nav className="container mx-auto flex items-center justify-between flex-wrap py-6">
@@ -12,11 +20,11 @@ const Nav = () => {
                     <img className='h-10 ml-1' src="assets/logo.webp" alt="" />
                 </Link>
                 <div className="block lg:hidden">
-                    <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
+                    <button onClick={collaps} className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
                         <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                     </button>
                 </div>
-                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:ml-10">
+                <div className={`${!open? "hidden" : "block"} w-full flex-grow lg:flex lg:items-center lg:w-auto lg:ml-10`}>
                     <div className="text-sm lg:flex-grow">
                         <Link to="add-task" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-6 font-semibold text-lg">
                             Add task
